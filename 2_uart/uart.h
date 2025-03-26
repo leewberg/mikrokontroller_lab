29858ed866f5a5968db717cec3a6130f8a2ea60e
Nicolay Lindal <nicolapl@Epimetheus.>
2025-03-26 15:40:33 +0100

    Added reserved spaces to the UART struct

--- 2_uart/uart.h
+++ 2_uart/uart.h
@@ -6,7 +6,6 @@
 #define RTSPIN 5
 #define CTSPIN 7
 #define UART_BASE 0x40002000
-#define RESERVED0_SIZE 0
 #define __BAUDRATE_VAL__ 0x00275000 //have to give BAUDRATE val of 0x00275000 (per page 542 in datablad) to get a baudrate of 9600
 /*RX on pin 8, TX on pin 6*/
 
@@ -17,44 +16,37 @@ typedef struct{
     volatile uint32_t TASKS_STOPRX;
     volatile uint32_t TASKS_STARTTX;
     volatile uint32_t TASTS_STOPTX;
-
-    volatile uint32_t RESERVED0[RESERVED0_SIZE];
-
+    volatile uint32_t RESERVED0[3]; // 0x01C - 0x010 = 28 - 16 = 12 = 3 ord
     volatile uint32_t TASKS_SUSPEND;
-
-    //RESERVED1
-
+    volatile uint32_t RESERVED1[56]; // enkelt
     volatile uint32_t EVENTS_CTS;
     volatile uint32_t EVENTS_NCTS;
     volatile uint32_t EVENTS_RXDRDY;
+    volatile uint32_t RESERVED2[4]; // 0x11C - 0x10C = 284 - 268 = 16 = 4 ord
     volatile uint32_t EVENTS_TXDRDY;
-
-    //RESERVED2
-
+    volatile uint32_t RESERVED3[1]; // 0x124 - 0x120 = 292 - 288 = 4 = 1 ord
     volatile uint32_t EVENTS_ERROR;
-
-    //RESERVED3
+    volatile uint32_t RESERVED[7]; // 0x144 - 0x128 = 68 - 40 = 28 = 7 ord
     volatile uint32_t EVENTS_RXTO;
-
-    //RESERVED4
+    volatile uint32_t RESERVED[46]; // 0x200 - 0x148 = 512 - 328 = 184 = 46 ord
     volatile uint32_t SHORTS;
-    //RESERVED5
+    volatile uint32_t RESERVED[63]; // 0x304 - 0x204 = 768 - 516 = 252 = 63 ord
     volatile uint32_t INTENSET;
     volatile uint32_t INTENCLR;
-    //RESERVED6
+    volatile uint32_t RESERVED[95]; // 0x480 - 0x30C = 1152 - 772 = 380 = 95 ord
     volatile uint32_t ERRORSRC;
-    //RESERVED7
+    volatile uint32_t RESERVED[31]; // 0x500 - 0x484 = 1280 - 1156 = 124 = 31 ord
     volatile uint32_t ENABLE;
-    //RESERVED8
+    volatile uint32_t RESERVED[1]; // duh
     volatile uint32_t PSELRTS;
     volatile uint32_t PSELTXD;
     volatile uint32_t PSELCTS;
     volatile uint32_t PSELRXD;
     volatile uint32_t REXD;
     volatile uint32_t TXD;
-    //RESERVED
+    volatile uint32_t RESERVED[1]; // 0x524 - 0x520 = 1316 ...
     volatile uint32_t BAUDRATE;
-    //RESERVED
+    volatile uint32_t RESERVED[17]; // 0x56C - 0x528 = 1388 - 1320 = 68 = 17 ord
     volatile uint32_t CONFIG;
 
 }NRF_UART_REG;
